c87df51097bc79110c7698c30562b1f6e0b84580
ct-bess <ct_bess@gmail.com>
2018-11-14 11:55:48 -0600

    satCheck broke it

--- DPLL/main.cpp
+++ DPLL/main.cpp
@@ -29,7 +29,9 @@ struct State {
   unsigned int backtraces = 0;
   bool satisfiability = false;
   Clause model; // our truth assignments
-  //vector<KnowledgeBase> KBtree // for backtracking
+  Clause pureSymbols;
+  //pair<vector<unsigned int>,Clause> pureSymbols;
+  vector<KnowledgeBase> KBtree; // for backtracking
   vector<char> modelOptions;
   // Unit clauses
   // Pure literals
@@ -42,7 +44,7 @@ void printKB( KnowledgeBase &KB ) {
   for( unsigned int i = 0; i < KB.size(); ++i ) {
 
     #if READABLE_PRINT
-    cout << i << ": ";
+    cout << i << ": (";
     #endif
 
     for( unsigned int j = 0; j < KB.at(i).size(); ++j ) {
@@ -60,7 +62,7 @@ void printKB( KnowledgeBase &KB ) {
     }
 
     #if READABLE_PRINT
-    cout << "\n";
+    cout << " )\n";
     #endif
 
   }
@@ -72,21 +74,34 @@ void printKB( KnowledgeBase &KB ) {
 }
 
 // FIXME UNTESTED
-bool satCheck( KnowledgeBase &KB, State &s ) {
+bool satCheck( KnowledgeBase &KB, State s ) {
 
-  // if every clause in KB is true in our model return true
-  for( unsigned int i = 0; i < KB.size(); ++i ) {
+  cout << "satCheck\n";
+  Clause unpureModel = s.model;
 
-    for( unsigned int j = 0; j < KB.at(i).size(); ++j ) {
+  cout << "PureCheck\n";
+  for( unsigned int n = 0; n < s.pureSymbols.size(); ++n ) {
+    for( unsigned int m = 0; m < unpureModel.size(); ++m ) {
+      if( unpureModel.at(m).second == s.pureSymbols.at(n).second ) { 
+        cout << "Found Pure\n";
+        unpureModel.erase( unpureModel.begin() + m );
+      }
+    }
+  }
 
-      // Loop thru model type Clause ==> FIXME might be better as outer loop
-      for( unsigned int m = 0; m < s.model.size(); ++m ) {
+  if( unpureModel.empty() ) return( false );
 
-        if( s.model.at(i).second == KB.at(i).at(j).second ) {
-          if( s.model.at(i).first != KB.at(i).at(j).first ) {
+  // Loop thru model type Clause 
+  for( unsigned int m = 0; m < unpureModel.size(); ++m ) {
 
+    // if every clause in KB is true in our model return true
+    for( unsigned int i = 0; i < KB.size(); ++i ) {
+
+      for( unsigned int j = 0; j < KB.at(i).size(); ++j ) {
+
+        if( unpureModel.at(m).second == KB.at(i).at(j).second ) {
+          if( unpureModel.at(m).first != KB.at(i).at(j).first ) {
             return( false );
-
           }
 
         }
@@ -104,6 +119,8 @@ bool satCheck( KnowledgeBase &KB, State &s ) {
 // TESTED
 bool emptyClauseCheck( KnowledgeBase &KB ) {
 
+  cout << "emptyClauseCheck\n";
+
   for( unsigned int i = 0; i < KB.size(); ++i ) {
 
     if( KB.at(i).empty() == true ) return( true );
@@ -115,7 +132,11 @@ bool emptyClauseCheck( KnowledgeBase &KB ) {
 }
 
 // AKA: resolution ==> TESTED
-void unitPropogate( Literal &unitClause, KnowledgeBase &KB ) {
+void unitPropogate( const Literal &unitClause, KnowledgeBase &KB, bool pureSym ) {
+
+  // UNIT PROPOGATION
+  // -- Delets literals from inside a clause, bringing us closer to an empty clause
+  // -- Recall empty clause means unSat
 
   for( unsigned int i = 0; i < KB.size(); ++i ) {
 
@@ -123,6 +144,16 @@ void unitPropogate( Literal &unitClause, KnowledgeBase &KB ) {
 
       if( unitClause.second == KB.at(i).at(j).second ) {
 
+        // Pure symbol ==> full clause removal
+        if( pureSym == true ) { 
+          KB.at(i).erase( KB.at(i).begin(), KB.at(i).end() );
+          KB.at(i).push_back( Literal( true, '*' ) );
+        }
+
+        // regular unit clause ==> partial removal
+        else KB.at(i).erase( KB.at(i).begin() + j );
+
+        /*
         // Full clause removal ==> FIXME might not be needed
         if( unitClause.first == KB.at(i).at(j).first ) {
 
@@ -136,6 +167,7 @@ void unitPropogate( Literal &unitClause, KnowledgeBase &KB ) {
           KB.at(i).erase( KB.at(i).begin() + j );
 
         }
+        */
 
       }
 
@@ -147,10 +179,109 @@ void unitPropogate( Literal &unitClause, KnowledgeBase &KB ) {
 
 }
 
+Literal findUnitClause( KnowledgeBase &KB, State &s ) {
+
+  // UNIT CLAUSE (heuristic)
+  // -- Observation that a clause contains 1 literal
+  // -- The model will define new unit clauses throughout DPLL
+  // -- e.g. if our model says B = true
+  // -- -- the clause (-B v -C) simplifies to (-C) ==> a unit clause
+  // -- -- thus we return C = false
+
+  // model = type Clause ==> vector<Literal> 
+
+  // 1: check for clause size == 1
+  for( unsigned int i = 0; i < KB.size(); ++i ) {
+
+    //if( KB.at(i).empty() ) break;
+
+    if( KB.at(i).size() == 1 ) return( KB.at(i).at(0) );
+  
+  }
+
+  // 2: use model to identify a unit clause
+  //  : : NOTE this might be done by calling unitPropogate on a temp KB
+  //  : : BUT only using the partial clause removal case
+  /* FIXME OMIT FOR NOW
+  for( unsigned int i = 0; i < KB.size(); ++i ) {
+
+    for( unsigned int j = 0; j < KB.at(i).size(); ++j ) {
+
+
+    }
+  
+  }
+  */
+
+  // on failure
+  return( Literal( false, '!' ) );
+
+}
+
+// TESTED but wtf...
+Literal findPureSymbol( KnowledgeBase &KB, State &s ) {
+
+  // PURE SYMBOL / LITERAL (heuristic)
+  // -- Observation that a literal occurs with only 1 polarity
+  // -- Only true or only false occurances of the variable in the KB
+  // -- Lets us set the pure variable to true and delete those entire clauses
+  // -- brings us closer to trivially sat
+
+  for( unsigned int m = 0; m < s.modelOptions.size(); ++m ) {
+
+    const char symbol = s.modelOptions.at(m);
+
+    bool setPolarity = false;
+    bool polarity;
+    bool pureSymbolFound = true;
+
+    for( unsigned int i = 0; i < KB.size(); ++i ) {
+
+      for( unsigned int j = 0; j < KB.at(i).size(); ++j ) {
+
+        if( KB.at(i).at(j).second == symbol ) {
+
+          if( setPolarity == false ) {
+            polarity = KB.at(i).at(j).first;
+            setPolarity = true;
+            continue;
+          }
+
+          if( KB.at(i).at(j).first != polarity ) {
+            pureSymbolFound = false;
+            break;
+          }
+
+        }
+
+      }
+
+    }
+
+    if( pureSymbolFound == true ) {
+      return( Literal( polarity, symbol ) );
+    }
+
+  }
+
+  // on failure
+  return( Literal( false, '!' ) );
+
+}
+
 // FIXME UNTESTED
 bool DPLL( KnowledgeBase &KB, State &s ) {
 
   s.DPLLcalls += 1;
+  s.KBtree.push_back( KB );
+
+  cout << "Model: { ";
+  for( unsigned int i = 0; i < s.model.size(); ++i ) {
+    if( s.model.at(i).first == false ) cout << "-";
+    else cout << " ";
+    cout << s.model.at(i).second << " ";
+  }
+  cout << " }" << endl;
 
   // 1: if KB is a consistent set of literals return true
   if( satCheck( KB, s ) == true ) return( true );
@@ -158,18 +289,47 @@ bool DPLL( KnowledgeBase &KB, State &s ) {
   // 2: if KB contains an empty clause return false
   if( emptyClauseCheck( KB ) == true ) return( false );
 
-  // 3: for each unit clause {uc} in KB
-  //      KB = unitPropagate( uc, KB )
+  // 3: Fine Pure Symbol
+  //  : : if we found 1, unitPropogate with pureSymbol BUT only delet the pure symbol
+  //  : : dont delet the whole clause that pure symbol is in.
+  //  : : mark the pure symbol = true in our model
+  //  : : return DPLL()
+  const Literal pureSymbol = findPureSymbol( KB, s );
 
-  // 4: for each pure literal {l} in KB
-  //      KB = pureLiteralAssign( l, KB )
+  // meaning it found a pure symbol
+  if( pureSymbol.second != '!' ) {
 
-  // 5: l = chooseLiteral( KB )
+    cout << "Pure Symbol: " << pureSymbol.first << " " << pureSymbol.second << "\n";
+    s.model.push_back( pureSymbol );
+    s.pureSymbols.push_back( pureSymbol );
+    //s.pureSymbols.second.push_back( pureSymbol );
+    unitPropogate( pureSymbol, KB, true );
+    printKB( KB );
+    return( DPLL( KB, s ) );
+
+  }
+
+  // 4: Find Unit Clause
+  //  : : Same as pure symbol case, but now for a unit clause
+  //  : : return DPLL() after the unit propogation
+
+  const Literal unitClause = findUnitClause( KB, s );
+
+  if( unitClause.second != '!' ) {
+
+    cout << "Unit Clause: " << unitClause.first << " " << unitClause.second << "\n";
+    s.model.push_back( unitClause );
+    unitPropogate( unitClause, KB, false );
+    printKB( KB );
+    return( DPLL( KB, s ) );
+
+  }
+
+  // 5: else choose a literal in model options that hasnt been chosen yet
 
   if( s.model.empty() ) s.model.push_back( Literal( true, s.modelOptions.front() ) );
   //                                           this ^^^^ truth doesn't matter
 
-  // FIXME wtf is this doing?
   else {
 
     s.model.push_back( Literal( true, s.modelOptions.at( s.model.size() - 1 ) ) );
@@ -177,25 +337,15 @@ bool DPLL( KnowledgeBase &KB, State &s ) {
   }
 
   const Literal chosenLit = s.model.back();
+  //s.model.push_back( chosenLit ); // allready in
 
   // 6: return( DPLL( KB && l ) or DPLL( KB && !l ) )
-  
-  // if no backtrack do true of chosenLit
+  //  : : FIXME backtracking
 
-  // else do false of chosenLit
-
-  // UNITPROP
-  // -- Delets literals from inside a clause, bringing us closer to an empty clause
-  // -- Recall empty clause means unSat
-
-  // PURELIT
-  // -- Observation that a literal occurs with only 1 polarity
-  // -- Only true or only false occurances of the variable in the KB
-  // -- Lets us set the pure variable to true and delete those entire clauses
-  // -- brings us closer to trivially sat
-
-  // FIXME -- PLACEHOLDER --
-  return( false );
+  cout << "Chose: " << chosenLit.first << " " << chosenLit.second << "\n";
+  unitPropogate( chosenLit, KB, false );
+  printKB( KB );
+  return( DPLL( KB, s ) );
 
 }
 
@@ -293,11 +443,20 @@ int main( int argc, char** argv ) {
   KnowledgeBase KB;
   readFile( problemName, KB, s );
 
-  cout << "Props: ";
-  for( auto w : s.modelOptions ) cout <<  w << " ";
+  cout << "Knowledge Base\nSymbols: ";
+  for( auto w : s.modelOptions ) cout << w << " ";
   cout << endl;
 
   printKB( KB );
+
+  s.satisfiability = DPLL( KB, s );
+
+  cout << "Satisfiable? : " << s.satisfiability << endl;
+
+  /*
+  Literal ps = findPureSymbol( KB, s );
+  cout << "PureLit: " << ps.first << " " << ps.second << endl;
+
   Literal p = make_pair( true, 'A' );
   unitPropogate( p, KB );
   printKB( KB );
@@ -305,6 +464,7 @@ int main( int argc, char** argv ) {
   unitPropogate( p, KB );
   printKB( KB );
   cout << "Empty Clause?: " << emptyClauseCheck( KB ) << endl;
+  */
 
   return( 0x5F3759DF );
 
